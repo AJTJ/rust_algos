6224291c6677bab42ab04fe71359d8d354dd6c59
AJTJ <janke.aaron@gmail.com>
2024-05-13 23:16:58 +0800

    implemented traversal functions

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,7 @@
-how to run one file or test
+how to run/watch files/tests from the CRATE level
 `cargo test -p binary_tree`
 `cargo run -p binary_tree`
+`cargo watch -x run`
+`cargo watch --exec test`
 
-how to cargo watch?
+how to cargo watch 
--- binary_tree/src/lib.rs
+++ binary_tree/src/lib.rs
@@ -1,50 +1,104 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct BinaryNode {
     val: i32,
     left: Option<Box<BinaryNode>>,
     right: Option<Box<BinaryNode>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct BinaryTree {
-    root: Box<BinaryNode>,
+    root: Option<Box<BinaryNode>>,
 }
 
 impl BinaryTree {
-    pub fn new(node: BinaryNode) -> BinaryTree {
+    pub fn new(node: Option<BinaryNode>) -> BinaryTree {
         BinaryTree {
-            root: Box::new(node),
+            root: node.map(|n| Box::new(n)),
         }
     }
 
     pub fn fill_ordered(&mut self, val: i32) {
-        let mut current = Some(&mut self.root);
+        if self.root.is_none() {
+            self.root = Some(Box::new(BinaryNode {
+                val,
+                left: None,
+                right: None,
+            }));
+        } else {
+            let mut current = &mut self.root;
 
-        while let Some(cur_box) = current {
-            if val > cur_box.val {
-                if cur_box.right.is_none() {
-                    cur_box.right = Some(Box::new(BinaryNode {
-                        val,
-                        left: None,
-                        right: None,
-                    }));
-                    break;
+            while let Some(cur_box) = current {
+                if val > cur_box.val {
+                    if cur_box.right.is_none() {
+                        cur_box.right = Some(Box::new(BinaryNode {
+                            val,
+                            left: None,
+                            right: None,
+                        }));
+                        break;
+                    } else {
+                        current = &mut cur_box.right;
+                    }
                 } else {
-                    current = cur_box.right.as_mut();
-                }
-            } else {
-                if cur_box.left.is_none() {
-                    cur_box.left = Some(Box::new(BinaryNode {
-                        val,
-                        left: None,
-                        right: None,
-                    }));
-                    break;
-                } else {
-                    current = cur_box.left.as_mut();
+                    if cur_box.left.is_none() {
+                        cur_box.left = Some(Box::new(BinaryNode {
+                            val,
+                            left: None,
+                            right: None,
+                        }));
+                        break;
+                    } else {
+                        current = &mut cur_box.left;
+                    }
                 }
             }
         }
+    }
+
+    pub fn in_order_traversal(&self) -> Vec<i32> {
+        let mut return_val = vec![];
+
+        fn traverse(n: &Option<Box<BinaryNode>>, v: &mut Vec<i32>) {
+            if let Some(node) = n {
+                traverse(&node.left, v);
+                v.push(node.val);
+                traverse(&node.right, v);
+            }
+        }
+
+        traverse(&self.root, &mut return_val);
+
+        return_val
+    }
+    pub fn pre_order_traversal(&self) -> Vec<i32> {
+        let mut return_val = vec![];
+
+        fn traverse(n: &Option<Box<BinaryNode>>, v: &mut Vec<i32>) {
+            if let Some(node) = n {
+                v.push(node.val);
+                traverse(&node.left, v);
+                traverse(&node.right, v);
+            }
+        }
+
+        traverse(&self.root, &mut return_val);
+
+        return_val
+    }
+    pub fn post_order_traversal(&self) -> Vec<i32> {
+        let mut return_val = vec![];
+
+        fn traverse(n: &Option<Box<BinaryNode>>, v: &mut Vec<i32>) {
+            if let Some(node) = n {
+                traverse(&node.left, v);
+                traverse(&node.right, v);
+                v.push(node.val);
+            }
+        }
+
+        traverse(&self.root, &mut return_val);
+
+        return_val
     }
 }
 
@@ -54,18 +108,26 @@ mod tests {
 
     #[test]
     fn test_ordering() {
-        let root_node = BinaryNode {
-            val: 5,
-            left: None,
-            right: None,
-        };
+        let mut my_tree = BinaryTree::new(None);
+        let test_val = vec![5, 3, 6, 2, 7, 4];
 
-        let mut my_tree = BinaryTree::new(root_node);
-
-        vec![3, 6, 2, 7, 4]
-            .iter()
-            .for_each(|x| my_tree.fill_ordered(*x));
+        test_val.iter().for_each(|x| my_tree.fill_ordered(*x));
 
         println!("my tree root: {:?}", my_tree.root);
+        assert_eq!(my_tree.clone().root.unwrap().val, 5);
+        assert_eq!(
+            &my_tree.root.clone().unwrap().left.as_ref().unwrap().val,
+            &3
+        );
+
+        let mut sorted_test_vals = test_val.clone();
+        sorted_test_vals.sort();
+        let in_order = my_tree.in_order_traversal();
+        let pre_order = my_tree.pre_order_traversal();
+        let post_order = my_tree.post_order_traversal();
+        println!("in_order: {in_order:?}");
+        println!("pre_order: {pre_order:?}");
+        println!("post_order: {post_order:?}");
+        assert_eq!(in_order, sorted_test_vals);
     }
 }
